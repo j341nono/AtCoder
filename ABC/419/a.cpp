ed75da3520ca3d16bb5bc04a9a7db290f5296edf
j341nono <kanakana.nono1224@gmail.com>
2025-08-23 23:26:10 +0900

    419-1

--- ABC/419/a.cpp
+++ ABC/419/a.cpp
@@ -3,5 +3,11 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
 int main(){
-
+    string s;
+    cin>>s;
+    if(s=="red")cout<<"SSS";
+    else if(s=="blue")cout<<"FFF";
+    else if(s=="green")cout<<"MMM";
+    else cout<<"Unknown";
+    cout<<endl;
 }
